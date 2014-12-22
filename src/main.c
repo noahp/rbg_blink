a12a98bc526b5f8ac59071befd3ab36d4dcf4bbc
np <np@np.np>
2014-12-22 15:01:26 -0500

    - remove rgb code

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@ static void main_init_io(void)
     // set B0 DDR to output
     GPIOB_PDDR |= (1 << 0);
 }
+
 static void main_spi_send(uint8_t *pData, uint32_t len)
 {
     // wait until dma busy flag is cleared
@@ -92,98 +93,6 @@ static void main_init_spi(void)
 
 }
 
-typedef union rgbData_u {
-    uint32_t all;
-    uint8_t byte[4];
-    struct {
-        uint8_t g;
-        uint8_t r;
-        uint8_t b;
-        uint8_t pad;
-    } color;
-}rgbData_t;
-
-//
-//  encodeWS2812B - encode 8 bits into 24 bits. pOut is 3x pIn length. len is
-//                  bytes.
-//
-static void encodeWS2812B(uint8_t *pIn, uint8_t *pOut, uint32_t len)
-{
-    uint8_t i,c;
-    uint32_t j;
-    rgbData_t output;
-
-    for(j=0; j<len; j++){
-        output.all = 0;
-        c = pIn[j];
-        // encode 1 byte into 3.
-        for(i=0; i<8; i++){
-            if(c & 0x80){
-                output.all |= (0x6);
-            }
-            else{
-                output.all |= (0x4);
-            }
-            c <<= 1;
-            output.all <<= 3;
-        }
-
-        output.all >>= 3;
-
-        pOut[3*j+0] = output.byte[2];
-        pOut[3*j+1] = output.byte[1];
-        pOut[3*j+2] = output.byte[0];
-    }
-}
-
-static uint8_t rawData[169];
-static rgbData_t rgbData = {.color = {.g=0xFF, .r=0x00, .b=0x00}};
-static uint32_t dataOff = 0xFF;
-static void main_increment_lights(void)
-{
-    // set the rgb values
-    if(dataOff < 0xFF){
-        rgbData.color.b--;
-        rgbData.color.g++;
-    }
-    else if (dataOff < 0xFF * 2){
-        // steady for 1 period
-        asm("nop");
-    }
-    else if(dataOff < 0xFF * 3){
-        rgbData.color.g--;
-        rgbData.color.r++;
-        if(rgbData.color.r > 0xFF /2){
-            asm("nop");
-        }
-    }
-    else if (dataOff < 0xFF * 4){
-        // steady for 1 period
-        asm("nop");
-    }
-    else if(dataOff < 0xFF * 5){
-        rgbData.color.r--;
-        rgbData.color.b++;
-    }
-    else if (dataOff < 0xFF * 6){
-        // steady for 1 period
-        asm("nop");
-    }
-
-    dataOff = (dataOff + 1) % (0xFF * 6);
-}
-
-void main_send_lights(void)
-{
-    // main_increment_lights();
-
-    // encode the sequence
-    encodeWS2812B(rgbData.byte, &rawData[160], 3);
-
-    // send the data
-    main_spi_send(rawData, sizeof(rawData));
-}
-
 static void main_led(void)
 {
     static uint32_t blinkTime = 0;
@@ -196,17 +105,6 @@ static void main_led(void)
     }
 }
 
-static void main_rgb(void)
-{
-    static uint32_t rgbTime = 0;
-
-    // refresh every 50 ms
-    if(systick_getMs() - rgbTime > 50){
-        rgbTime = systick_getMs();
-        main_send_lights();
-    }
-}
-
 int main(void)
 {
     uint8_t cdcChar;
@@ -215,9 +113,6 @@ int main(void)
     main_init_io();
     main_init_spi();
 
-    // init buffer for spi packed data- fixed low pulse of 160 byte-times
-    memset(rawData, 0, sizeof(rawData));
-
     // usb init
     usb_main_init();
 
@@ -225,31 +120,9 @@ int main(void)
         // led task
         main_led();
 
-        // rgb task
-        main_rgb();
-
         // usb task
         if(usb_main_mainfunction(&cdcChar) != -1){
             switch(cdcChar){
-                case 'r':
-                    rgbData.all = 0;
-                    rgbData.color.r = 0xFF;
-                    break;
-
-                case 'g':
-                    rgbData.all = 0;
-                    rgbData.color.g = 0xFF;
-                    break;
-
-                case 'b':
-                    rgbData.all = 0;
-                    rgbData.color.b = 0xFF;
-                    break;
-
-                case 'o':
-                    rgbData.all = 0;
-                    break;
-
                 default:
                     break;
             }
