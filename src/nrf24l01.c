80ecdfc2f456dab95c26aacf439cc80f748375fc
np <np@np.np>
2014-12-22 15:12:02 -0500

    - add blank files

---
+++ src/nrf24l01.c
@@ -0,0 +1,7 @@
+
+//
+// nrf24l01.c
+//
+//  Interface (driver) to nrf24l01+ module. User must register spi_tx & spi_rx functions.
+//
+#include "nrf24l01.h"
---
+++ src/nrf24l01.h
@@ -0,0 +1,7 @@
+
+//
+// nrf24l01.h
+//
+//  API for nrf24l01.c, which is the interface to the nrf24l01+ modules.
+//
+#include <stdint.h>
